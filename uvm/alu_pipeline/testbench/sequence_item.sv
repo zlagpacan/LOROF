3685e35c2fcd8c069be565091e85ef8ae85b59ac
AdamKeith1 <122839729+AdamKeith1@users.noreply.github.com>
2024-12-23 18:18:18 -0500

    alu_pipeline tx

--- uvm/alu_pipeline/testbench/sequence_item.sv
+++ uvm/alu_pipeline/testbench/sequence_item.sv
@@ -21,8 +21,31 @@ class alu_pipeline_seq_item extends uvm_sequence_item
     // --- Reset --- //
     logic nRST;
 
-    // --- OP Issue : Signals --- //
-    randc logic [3:0] op_in;
+    // --- In : Control Signals --- //
+    randc logic valid_in; // TODO: constraints
+    randc logic A_unneeded_in;
+    randc logic A_forward_in;
+    randc logic B_forward_in;
+    randc logic A_reg_read_valid_in;
+    randc logic B_reg_read_valid_in;
+
+    randc logic [3:0]                    op_in;
+    randc logic [LOG_PRF_BANK_COUNT-1:0] A_bank_in;
+    randc logic [LOG_PRF_BANK_COUNT-1:0] B_bank_in; 
+    randc logic [LOG_PR_COUNT-1:0]       dest_PR_in; // unsure on rand or randc
+
+    // --- In : Fully Rand --- //
+    rand  logic                      [31:0] imm_in;
+    rand  logic [PRF_BANK_COUNT-1:0] [31:0] reg_read_data_by_bank_in;
+    rand  logic [PRF_BANK_COUNT-1:0] [31:0] forward_data_by_bank_in;
+
+    // --- Outputs --- //
+    logic ready_out;
+    logic WB_valid_out;
+
+    logic [31:0]             WB_data_out;
+    logic [LOG_PR_COUNT-1:0] WB_PR_out;
+
 
     // --- OP Issue : Constraints --- //
     constraint op_in_range {
@@ -41,4 +64,11 @@ class alu_pipeline_seq_item extends uvm_sequence_item
         };
     }
 
+    // --- Constructor --- //
+    function new(string name = "alu_pipeline_seq_item");
+        super.new(name);
+    endfunction : new
+
 endclass : alu_pipeline_seq_item
+
+`endif
