8eeed0d6b84e058669082c1a70e49ccf6a5da113
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2024-12-26 21:07:52 -0500

    scoreboard scaffolding

--- uvm/alu_pipeline/testbench/scoreboard.sv
+++ uvm/alu_pipeline/testbench/scoreboard.sv
@@ -21,9 +21,60 @@ class alu_pipeline_scoreboard extends uvm_scoreboard;
     `uvm_component_utils(alu_pipeline_scoreboard)
 
     // --- UVM Analysis Port --- //
+    uvm_analysis_imp #(alu_pipeline_seq_item, alu_pipeline_scoreboard) alu_pipeline_scbd_ap;
+    alu_pipeline_seq_item transactions[$];
+
+    // --- Constructor --- //
+    function new (string name = "alu_pipeline_scoreboard", uvm_component parent);
+        super.new(name, parent);
+        `uvm_info("alu_pipeline_scoreboard", "Constructor", UVM_HIGH)
+    endfunction : new
     
+    // --- Build Phase --- //
+    function void build_phase (uvm_phase phase);
+        super.build_phase(phase);
+        `uvm_info("alu_pipeline_scoreboard", "Build Phase", UVM_HIGH)
+    
+        // --- Scoreboard Port --- //
+        alu_pipeline_scbd_ap = new("alu_pipeline_scbd_ap", this);
+        
+    endfunction : build_phase
 
+    // --- Connect Phase --- //
+    function void connect_phase (uvm_phase phase);
+        super.connect_phase(phase);
+        `uvm_info("alu_pipeline_scoreboard", "Connect Phase", UVM_HIGH)
+    
+    endfunction : connect_phase
+    
+    // --- Write Sequence Item --- //
+    function void write (alu_pipeline_seq_item alu_pipeline_tx);
+        transactions.push_back(alu_pipeline_tx);
+    endfunction : write 
 
+    // --- Run Phase --- //
+    task run_phase (uvm_phase phase);
+        super.run_phase(phase);
+        `uvm_info("alu_pipeline_scoreboard", "Run Phase", UVM_HIGH)
+    
+        // --- Sequence Item Stack --- //
+        forever begin
+        alu_pipeline_seq_item curr_trans;
+        wait((transactions.size() != 0));
+        curr_trans = transactions.pop_front();
+        compare(curr_trans);
+        end
+        
+    endtask : run_phase
+
+    // --- Compare --- //
+    task compare (alu_pipeline_seq_item curr_trans);
+
+        // --- Actual vs Expected --- //
+
+        // --- SVA? -- //
+
+    endtask : compare
 
 endclass : alu_pipeline_scoreboard
 
