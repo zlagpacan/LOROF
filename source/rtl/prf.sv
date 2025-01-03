ac2264a8aa6daf6baf1bff5ad29013a67373a4f0
Zach Lagpacan <zlagpaca@purdue.edu>
2025-01-03 15:09:16 -0500

    prf progress

--- source/rtl/prf.sv
+++ source/rtl/prf.sv
@@ -32,7 +32,7 @@ module prf (
     input logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]     WB_ROB_index_by_wr,
 
     // writeback backpressure by write requestor
-    output logic WB_ready_by_wr
+    output logic [PRF_WR_COUNT-1:0] WB_ready_by_wr
 
     // writeback bus by bank
     output logic [PRF_BANK_COUNT-1:0]                                       WB_bus_valid_by_bank,
@@ -46,12 +46,87 @@ module prf (
     logic [PRF_BANK_COUNT-1:0][PR_COUNT/PRF_BANK_COUNT-1:0][31:0] PRF_by_bank;
 
     // Read Req Signals
-    logic [PRF_RR_COUNT-1:0]                unacked_reg_read_req_valid_by_rr;
-    logic [PRF_RR_COUNT-1:0][LOG_PR_COUNT]  unacked_reg_read_req_PR_by_rr;
-    logic [PRF_RR_COUNT-1:0]                compressed_reg_read_req_valid_by_rr;
-    logic [PRF_RR_COUNT-1:0]                compressed_reg_read_req_PR_by_rr;
-    logic [PRF_RR_COUNT-1:0]                last_reg_read_mask;
+    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    reg_read_req_valid_by_bank_by_rr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    unacked_reg_read_req_valid_by_bank_by_rr;
+    logic [PRF_RR_COUNT-1:0][LOG_PR_COUNT-1:0]      unacked_reg_read_req_PR_by_rr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    compressed_reg_read_req_valid_by_bank_by_rr;
+    logic [PRF_RR_COUNT-1:0][LOG_PR_COUNT-1:0]      compressed_reg_read_req_PR_by_rr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    last_reg_read_mask_by_bank;
 
     // Write Req Signals
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    WB_valid_by_bank_by_wr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    unacked_WB_valid_by_bank_by_wr;
+    logic [PRF_WR_COUNT-1:0][31:0]                  unacked_WB_data_by_wr,
+    logic [PRF_WR_COUNT-1:0][LOG_PR_COUNT-1:0]      unacked_WB_PR_by_wr,
+    logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]   unacked_WB_ROB_index_by_wr,
+
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    compressed_WB_valid_by_bank_by_wr;
+    logic [PRF_WR_COUNT-1:0][31:0]                  compressed_WB_data_by_wr,
+    logic [PRF_WR_COUNT-1:0][LOG_PR_COUNT-1:0]      compressed_WB_PR_by_wr,
+    logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]   compressed_WB_ROB_index_by_wr,
+
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    last_WB_mask;
+
+    // ----------------------------------------------------------------
+    // Reg Read Logic:
+
+    always_comb begin
+
+        // demux reg read req's to banks
+        reg_read_req_valid_by_bank_by_rr = '0;
+        for (int rr = 0; rr < PRF_RR_COUNT; rr++) begin
+            reg_read_req_valid_by_bank_by_rr[reg_read_req_PR_by_rr[rr][LOG_PRF_BANK_COUNT-1:0]][rr] = reg_read_req_valid_by_rr[rr];
+        end
+
+        // compress current + unacked req's
+        compressed_reg_read_req_valid_by_bank_by_rr = reg_read_req_valid_by_bank_by_rr | unacked_reg_read_req_valid_by_bank_by_rr;
+
+        // select current vs. unacked req PR for compressed
+        for (int rr = 0; rr < PRF_RR_COUNT; rr++) begin
+
+            // use unacked reg read PR if any bank valid for unacked reg read
+            if (|unacked_reg_read_req_valid_by_bank_by_rr[PRF_BANK_COUNT-1:0][rr]) begin
+                compressed_reg_read_req_PR_by_rr[rr] = unacked_reg_read_req_PR_by_rr;
+            end
+            else begin
+                compressed_reg_read_req_PR_by_rr[rr] = reg_read_req_PR_by_rr;
+            end
+        end
+    end
+
+    // FF
+    always_ff @ (posedge CLK, negedge nRST) begin
+        if (~nRST) begin
+            unacked_reg_read_req_valid_by_bank_by_rr <= '0;
+            unacked_reg_read_req_PR_by_rr <= '0;
+            last_reg_read_mask_by_bank <= '0;
+        end
+        else begin
+
+        end
+    end
+
+    // determine ack's for next cycle
+
+    // ----------------------------------------------------------------
+    // Writeback Logic:
+
+    // assign WB_ready_by_wr = WB_valid_by_wr & unacked_WB_valid_by_wr;
+
+    // next unacked = ready?
+
+    // FF
+    always_ff @ (posedge CLK, negedge nRST) begin
+        if (~nRST) begin
+
+        end
+        else begin
+
+        end
+    end
 
 endmodule
