f6fcc3c699b17e3582cdcf8a81a67e4a5c8ce747
Zach Lagpacan <zlagpaca@purdue.edu>
2025-01-05 01:29:25 -0500

    prf compiling no errors

--- source/rtl/prf.sv
+++ source/rtl/prf.sv
@@ -49,18 +49,29 @@ module prf (
 
     // Reg File RAM Array
     logic [PRF_BANK_COUNT-1:0][PR_COUNT/PRF_BANK_COUNT-1:0][31:0]       prf_array_by_bank;
+
     logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     prf_port0_read_upper_PR_by_bank;
     logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     next_prf_port0_read_upper_PR_by_bank;
     logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     prf_port1_read_upper_PR_by_bank;
     logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     next_prf_port1_read_upper_PR_by_bank;
+    
+    logic [PRF_BANK_COUNT-1:0]                                          prf_WB_valid_by_bank;
+    logic [PRF_BANK_COUNT-1:0]                                          next_prf_WB_valid_by_bank;
+    logic [PRF_BANK_COUNT-1:0][31:0]                                    prf_WB_data_by_bank;
+    logic [PRF_BANK_COUNT-1:0][31:0]                                    next_prf_WB_data_by_bank;
+    logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     prf_WB_upper_PR_by_bank;
+    logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0]     next_prf_WB_upper_PR_by_bank;
+    logic [PRF_BANK_COUNT-1:0][LOG_ROB_ENTRIES-1:0]                     prf_WB_ROB_index_by_bank; 
+    logic [PRF_BANK_COUNT-1:0][LOG_ROB_ENTRIES-1:0]                     next_prf_WB_ROB_index_by_bank;
+        // ROB_index also here since needs to use one-hot mux logic
 
     // Read Req Signals
-    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    reg_read_req_valid_by_bank_by_rr;
-
     logic [PRF_RR_COUNT-1:0]                        unacked_reg_read_req_valid_by_rr;
     logic [PRF_RR_COUNT-1:0]                        next_unacked_reg_read_req_valid_by_rr;
     logic [PRF_RR_COUNT-1:0][LOG_PR_COUNT-1:0]      unacked_reg_read_req_PR_by_rr;
     logic [PRF_RR_COUNT-1:0][LOG_PR_COUNT-1:0]      next_unacked_reg_read_req_PR_by_rr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    reg_read_req_valid_by_bank_by_rr;
     logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    unacked_reg_read_req_valid_by_bank_by_rr;
 
     logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    compressed_reg_read_req_valid_by_bank_by_rr;
@@ -77,13 +88,17 @@ module prf (
     logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    last_reg_read_mask_by_bank;
     logic [PRF_BANK_COUNT-1:0][PRF_RR_COUNT-1:0]    next_last_reg_read_mask_by_bank;
 
-    // Write Req Signals
-    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    WB_valid_by_bank_by_wr;
-
+    // Writeback Signals
     logic [PRF_WR_COUNT-1:0]                        unacked_WB_valid_by_wr;
+    logic [PRF_WR_COUNT-1:0]                        next_unacked_WB_valid_by_wr;
     logic [PRF_WR_COUNT-1:0][31:0]                  unacked_WB_data_by_wr;
+    logic [PRF_WR_COUNT-1:0][31:0]                  next_unacked_WB_data_by_wr;
     logic [PRF_WR_COUNT-1:0][LOG_PR_COUNT-1:0]      unacked_WB_PR_by_wr;
+    logic [PRF_WR_COUNT-1:0][LOG_PR_COUNT-1:0]      next_unacked_WB_PR_by_wr;
     logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]   unacked_WB_ROB_index_by_wr;
+    logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]   next_unacked_WB_ROB_index_by_wr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    WB_valid_by_bank_by_wr;
     logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    unacked_WB_valid_by_bank_by_wr;
 
     logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    compressed_WB_valid_by_bank_by_wr;
@@ -91,7 +106,13 @@ module prf (
     logic [PRF_WR_COUNT-1:0][LOG_PR_COUNT-1:0]      compressed_WB_PR_by_wr;
     logic [PRF_WR_COUNT-1:0][LOG_ROB_ENTRIES-1:0]   compressed_WB_ROB_index_by_wr;
 
-    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    last_WB_mask;
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    masked_WB_ack_by_bank_by_wr;
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    unmasked_WB_ack_by_bank_by_wr;
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    WB_ack_by_bank_by_wr;
+    logic [PRF_BANK_COUNT-1:0]                      WB_ack_by_wr;
+
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    last_WB_mask_by_bank;
+    logic [PRF_BANK_COUNT-1:0][PRF_WR_COUNT-1:0]    next_last_WB_mask_by_bank;
 
     // ----------------------------------------------------------------
     // Reg Read Logic:
@@ -103,6 +124,7 @@ module prf (
         RR_PQ = '0;
 
         // go through req vec
+            // lsb to msb so msb gets last say
         for (int rr = 0; rr < PRF_RR_COUNT; rr++) begin
             
             // check this req hot
@@ -114,6 +136,7 @@ module prf (
                 RR_PQ[rr] = 1'b1;
             end
         end
+
     endfunction
 
     always_comb begin
@@ -156,7 +179,7 @@ module prf (
             // select port 0:
                 // if any masked req, use masked
                 // else use unmasked
-            if (|compressed_reg_read_req_valid_by_bank_by_rr[bank] & last_reg_read_mask_by_bank[bank]) begin
+            if (|(compressed_reg_read_req_valid_by_bank_by_rr[bank] & last_reg_read_mask_by_bank[bank])) begin
                 port0_reg_read_ack_by_bank_by_rr[bank] = port0_masked_reg_read_ack_by_bank_by_rr[bank];
             end
             else begin
@@ -175,7 +198,7 @@ module prf (
             // select port 1:
                 // if any masked req, use masked
                 // else use unmasked
-            if (|compressed_reg_read_req_valid_by_bank_by_rr[bank] & ~port0_reg_read_ack_by_bank_by_rr[bank] & last_reg_read_mask_by_bank[bank]) begin
+            if (|(compressed_reg_read_req_valid_by_bank_by_rr[bank] & ~port0_reg_read_ack_by_bank_by_rr[bank] & last_reg_read_mask_by_bank[bank])) begin
                 port1_reg_read_ack_by_bank_by_rr[bank] = port1_masked_reg_read_ack_by_bank_by_rr[bank];
             end
             else begin
@@ -198,7 +221,7 @@ module prf (
         for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
             next_unacked_reg_read_req_valid_by_rr |= compressed_reg_read_req_valid_by_bank_by_rr[bank];
         end
-        next_unacked_reg_read_req_valid_by_rr = next_unacked_reg_read_req_valid_by_rr & ~reg_read_ack_by_rr;
+        next_unacked_reg_read_req_valid_by_rr &= ~reg_read_ack_by_rr;
 
         next_unacked_reg_read_req_PR_by_rr = compressed_reg_read_req_PR_by_rr;
 
@@ -206,7 +229,7 @@ module prf (
             // base on port 1
         next_last_reg_read_mask_by_bank = '0;
         for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
-            for (int rr = PRF_BANK_COUNT-2; rr >= 0; rr++) begin
+            for (int rr = PRF_RR_COUNT-2; rr >= 0; rr++) begin
                 next_last_reg_read_mask_by_bank[bank][rr] = reg_read_ack_by_rr[rr+1] | next_last_reg_read_mask_by_bank[bank][rr+1];
             end
         end
@@ -274,10 +297,193 @@ module prf (
     // ----------------------------------------------------------------
     // Writeback Logic:
 
-    // assign WB_ready_by_wr = WB_valid_by_wr & unacked_WB_valid_by_wr;
+    // PQ function for WR's, prioritizing msb to lsb
+    function logic [PRF_WR_COUNT-1:0] WR_PQ (input [PRF_WR_COUNT-1:0] req_vec);
 
-    // next unacked = ready?
+        // init clear vec
+        WR_PQ = '0;
 
+        // go through req vec
+            // lsb to msb so msb gets last say
+        for (int wr = 0; wr < PRF_WR_COUNT; wr++) begin
+            
+            // check this req hot
+            if (req_vec[wr]) begin
+
+                // new one-hot vec
+                    // override any lsb one-hot
+                WR_PQ = '0;
+                WR_PQ[wr] = 1'b1;
+            end
+        end
+
+    endfunction
+
+    always_comb begin
+
+        // demux WB's to banks
+        WB_valid_by_bank_by_wr = '0;
+        unacked_WB_valid_by_bank_by_wr = '0;
+        for (int wr = 0; wr < PRF_WR_COUNT; wr++) begin
+            WB_valid_by_bank_by_wr[WB_PR_by_wr[wr][LOG_PRF_BANK_COUNT-1:0]][wr] = WB_valid_by_wr[wr];
+            unacked_WB_valid_by_bank_by_wr[unacked_WB_PR_by_wr[wr][LOG_PRF_BANK_COUNT-1:0]][wr] = unacked_WB_valid_by_wr[wr];
+        end
+    
+        // compress current + unacked req's
+        compressed_WB_valid_by_bank_by_wr = WB_valid_by_bank_by_wr | unacked_WB_valid_by_bank_by_wr;
+
+        // select current vs. unacked req info for compressed
+        for (int wr = 0; wr < PRF_WR_COUNT; wr++) begin
+
+            // use unacked WB info if any bank valid for unacked WB
+            if (unacked_WB_valid_by_wr[wr]) begin
+                compressed_WB_data_by_wr[wr] = unacked_WB_data_by_wr[wr];
+                compressed_WB_PR_by_wr[wr] = unacked_WB_PR_by_wr[wr];
+                compressed_WB_ROB_index_by_wr[wr] = unacked_WB_ROB_index_by_wr[wr];
+            end
+            else begin
+                compressed_WB_data_by_wr[wr] = WB_data_by_wr[wr];
+                compressed_WB_PR_by_wr[wr] = WB_PR_by_wr[wr];
+                compressed_WB_ROB_index_by_wr[wr] = WB_ROB_index_by_wr[wr];
+            end
+        end
+
+        // ack PQ by bank
+        for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
+
+            // masked
+            masked_WB_ack_by_bank_by_wr = WR_PQ(compressed_WB_valid_by_bank_by_wr[bank] & last_WB_mask_by_bank[bank]);
+
+            // unmasked
+            unmasked_WB_ack_by_bank_by_wr = WR_PQ(compressed_WB_valid_by_bank_by_wr[bank]);
+        
+            // select masked vs. unmasked
+                // if any masked req, use masked
+                // else use unmasked
+            if (|(compressed_WB_valid_by_bank_by_wr[bank] & last_WB_mask_by_bank[bank])) begin
+                WB_ack_by_bank_by_wr[bank] = masked_WB_ack_by_bank_by_wr[bank];
+            end
+            else begin
+                WB_ack_by_bank_by_wr[bank] = unmasked_WB_ack_by_bank_by_wr[bank];
+            end
+        end
+
+        // final ack
+            // combine over all banks
+        WB_ack_by_wr = '0;
+        for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
+            WB_ack_by_wr |= WB_ack_by_bank_by_wr[bank];
+        end
+
+        // unacked req's
+        next_unacked_WB_valid_by_wr = '0;
+        for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
+            next_unacked_WB_valid_by_wr |= compressed_WB_valid_by_bank_by_wr[bank];
+        end
+        next_unacked_WB_valid_by_wr &= ~WB_ack_by_wr;
+
+        next_unacked_WB_data_by_wr = compressed_WB_data_by_wr;
+        next_unacked_WB_PR_by_wr = compressed_WB_PR_by_wr;
+        next_unacked_WB_ROB_index_by_wr = compressed_WB_ROB_index_by_wr;
+    
+        // last WB mask
+        next_last_WB_mask_by_bank = '0;
+        for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
+            for (int wr = PRF_WR_COUNT-2; wr >= 0; wr++) begin
+                next_last_WB_mask_by_bank[bank][wr] = WB_ack_by_wr[wr+1] | next_last_WB_mask_by_bank[bank][wr+1];
+            end
+        end
+    end
+
+    // ddetermine next reg WB
+    always_comb begin
+
+        // determine which banks hot
+
+
+        // go through banks
+        for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
+
+            // valid follows any compressed for bank
+            next_prf_WB_valid_by_bank[bank] = |compressed_reg_read_req_valid_by_bank_by_rr[bank];
+
+            // one-hot mux
+                // get data and PR:
+                    // AND with ACK
+                    // OR by wr
+            next_prf_WB_data_by_bank[bank] = '0;
+            next_prf_WB_upper_PR_by_bank[bank] = '0;
+            next_prf_WB_ROB_index_by_bank[bank] = '0;
+
+            for (int wr = 0; wr < PRF_WR_COUNT; wr++) begin
+                next_prf_WB_data_by_bank[bank] |= 
+                    compressed_WB_data_by_wr[wr]
+                    &
+                    {32{WB_ack_by_bank_by_wr[bank][wr]}}
+                ;
+                next_prf_WB_upper_PR_by_bank[bank] |=
+                    compressed_WB_PR_by_wr[wr][LOG_PR_COUNT-1:LOG_PRF_BANK_COUNT]
+                    &
+                    {(LOG_PR_COUNT-LOG_PRF_BANK_COUNT){WB_ack_by_bank_by_wr[bank][wr]}}
+                ;
+                next_prf_WB_ROB_index_by_bank[bank] |= 
+                    compressed_WB_ROB_index_by_wr[wr]
+                    &
+                    {LOG_ROB_ENTRIES{WB_ack_by_bank_by_wr[bank][wr]}}
+                ;
+            end
+        end
+    end
+
+    // ready logic
+        // ready if don't currently have double req
+        // if have double req:
+            // stall this cycle, guaranteed keeping current around
+            // once ack unacked req (can be this cycle or later cycle), can keep things moving starting the next cycle
+            // cycle after unacked is acked, can collect current or make it unacked
+        // not a true buffer, more of a stall when full this cycle
+            // will stall even if won't be full next cycle
+            // do this for friendlier timing, can know if WB ready early in cycle
+    assign WB_ready_by_wr = ~(WB_valid_by_wr & unacked_WB_valid_by_wr);
+
+    // writeback bus
+    assign WB_bus_valid_by_bank = prf_WB_valid_by_bank;
+    assign WB_bus_data_by_bank = prf_WB_data_by_bank;
+    assign WB_bus_upper_PR_by_bank = prf_WB_upper_PR_by_bank;
+    assign WB_bus_ROB_index_by_bank = prf_WB_ROB_index_by_bank;
+
+    // FF
+    always_ff @ (posedge CLK, negedge nRST) begin
+        if (~nRST) begin
+            prf_WB_valid_by_bank <= '0;
+            prf_WB_data_by_bank <= '0;
+            prf_WB_upper_PR_by_bank <= '0;
+            prf_WB_ROB_index_by_bank <= '0;
+            unacked_WB_valid_by_wr <= '0;
+            unacked_WB_data_by_wr <= '0;
+            unacked_WB_PR_by_wr <= '0;
+            unacked_WB_ROB_index_by_wr <= '0;
+            last_WB_mask_by_bank <= '0;
+            forward_data_by_bank <= '0;
+        end
+        else begin
+            prf_WB_valid_by_bank <= next_prf_WB_valid_by_bank;
+            prf_WB_data_by_bank <= next_prf_WB_data_by_bank;
+            prf_WB_upper_PR_by_bank <= next_prf_WB_upper_PR_by_bank;
+            prf_WB_ROB_index_by_bank <= next_prf_WB_ROB_index_by_bank;
+            unacked_WB_valid_by_wr <= next_unacked_WB_valid_by_wr;
+            unacked_WB_data_by_wr <= next_unacked_WB_data_by_wr;
+            unacked_WB_PR_by_wr <= next_unacked_WB_PR_by_wr;
+            unacked_WB_ROB_index_by_wr <= next_unacked_WB_ROB_index_by_wr;
+            last_WB_mask_by_bank <= next_last_WB_mask_by_bank;
+            forward_data_by_bank <= prf_WB_data_by_bank; 
+                // 1-cycle delay from when actual WB happened
+                    // this is cycle when pipelines can pick up value 
+                // cycle before is when WB info is advertized to IQs
+        end
+    end
+
+    // actual reg write
     // Distributed RAM
     always_ff @ (posedge CLK, negedge nRST) begin
         if (~nRST) begin
@@ -285,8 +491,8 @@ module prf (
         end
         else begin
             for (int bank = 0; bank < PRF_BANK_COUNT; bank++) begin
-                if (WB_bus_valid_by_bank[bank]) 
-                    prf_array_by_bank[bank][WB_bus_upper_PR_by_bank] <= WB_bus_data_by_bank;
+                if (prf_WB_valid_by_bank[bank]) 
+                    prf_array_by_bank[bank][prf_WB_upper_PR_by_bank] <= prf_WB_data_by_bank;
             end
         end
     end
