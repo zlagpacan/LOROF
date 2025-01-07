d6a51c0df7513f1e053ca941ba722e72a62c1ba1
Zach Lagpacan <zlagpaca@purdue.edu>
2025-01-07 18:09:17 -0500

    alu_iq and alu_pipeline_v2 wrappers updated

--- source/wrappers/alu_iq_wrapper.sv
+++ source/wrappers/alu_iq_wrapper.sv
@@ -28,6 +28,7 @@ module alu_iq_wrapper (
 	input logic [3:0] next_dispatch_is_imm_by_entry,
 	input logic [3:0] next_dispatch_B_ready_by_entry,
 	input logic [3:0][LOG_PR_COUNT-1:0] next_dispatch_dest_PR_by_entry,
+	input logic [3:0][LOG_ROB_ENTRIES-1:0] next_dispatch_ROB_index_by_entry,
 
     // ALU op dispatch feedback by entry
 	output logic [3:0] last_dispatch_open_by_entry,
@@ -35,9 +36,9 @@ module alu_iq_wrapper (
     // ALU pipeline feedback
 	input logic next_pipeline_ready,
 
-    // writeback bus
-	input logic [PRF_BANK_COUNT-1:0] next_WB_valid_by_bank,
-	input logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0] next_WB_upper_PR_by_bank,
+    // writeback bus by bank
+	input logic [PRF_BANK_COUNT-1:0] next_WB_bus_valid_by_bank,
+	input logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0] next_WB_bus_upper_PR_by_bank,
 
     // ALU op issue to ALU pipeline
 	output logic last_issue_valid,
@@ -50,6 +51,7 @@ module alu_iq_wrapper (
 	output logic last_issue_B_forward,
 	output logic [LOG_PRF_BANK_COUNT-1:0] last_issue_B_bank,
 	output logic [LOG_PR_COUNT-1:0] last_issue_dest_PR,
+	output logic [LOG_ROB_ENTRIES-1:0] last_issue_ROB_index,
 
     // reg read req to PRF
 	output logic last_PRF_req_A_valid,
@@ -73,6 +75,7 @@ module alu_iq_wrapper (
 	logic [3:0] dispatch_is_imm_by_entry;
 	logic [3:0] dispatch_B_ready_by_entry;
 	logic [3:0][LOG_PR_COUNT-1:0] dispatch_dest_PR_by_entry;
+	logic [3:0][LOG_ROB_ENTRIES-1:0] dispatch_ROB_index_by_entry;
 
     // ALU op dispatch feedback by entry
 	logic [3:0] dispatch_open_by_entry;
@@ -80,9 +83,9 @@ module alu_iq_wrapper (
     // ALU pipeline feedback
 	logic pipeline_ready;
 
-    // writeback bus
-	logic [PRF_BANK_COUNT-1:0] WB_valid_by_bank;
-	logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0] WB_upper_PR_by_bank;
+    // writeback bus by bank
+	logic [PRF_BANK_COUNT-1:0] WB_bus_valid_by_bank;
+	logic [PRF_BANK_COUNT-1:0][LOG_PR_COUNT-LOG_PRF_BANK_COUNT-1:0] WB_bus_upper_PR_by_bank;
 
     // ALU op issue to ALU pipeline
 	logic issue_valid;
@@ -95,6 +98,7 @@ module alu_iq_wrapper (
 	logic issue_B_forward;
 	logic [LOG_PRF_BANK_COUNT-1:0] issue_B_bank;
 	logic [LOG_PR_COUNT-1:0] issue_dest_PR;
+	logic [LOG_ROB_ENTRIES-1:0] issue_ROB_index;
 
     // reg read req to PRF
 	logic PRF_req_A_valid;
@@ -115,7 +119,7 @@ module alu_iq_wrapper (
 
 
 		    // ALU op dispatch by entry
-			dispatch_valid_by_entry <= 4'b0000;
+			dispatch_valid_by_entry <= '0;
 			dispatch_op_by_entry <= '0;
 			dispatch_imm_by_entry <= '0;
 			dispatch_A_PR_by_entry <= '0;
@@ -125,19 +129,20 @@ module alu_iq_wrapper (
 			dispatch_is_imm_by_entry <= '0;
 			dispatch_B_ready_by_entry <= '0;
 			dispatch_dest_PR_by_entry <= '0;
+			dispatch_ROB_index_by_entry <= '0;
 
 		    // ALU op dispatch feedback by entry
-			last_dispatch_open_by_entry <= 4'b1111;
+			last_dispatch_open_by_entry <= '0;
 
 		    // ALU pipeline feedback
-			pipeline_ready <= 1'b1;
+			pipeline_ready <= '0;
 
-		    // writeback bus
-			WB_valid_by_bank <= 4'b0000;
-			WB_upper_PR_by_bank <= '0;
+		    // writeback bus by bank
+			WB_bus_valid_by_bank <= '0;
+			WB_bus_upper_PR_by_bank <= '0;
 
 		    // ALU op issue to ALU pipeline
-			last_issue_valid <= 1'b0;
+			last_issue_valid <= '0;
 			last_issue_op <= '0;
 			last_issue_is_imm <= '0;
 			last_issue_imm <= '0;
@@ -147,11 +152,12 @@ module alu_iq_wrapper (
 			last_issue_B_forward <= '0;
 			last_issue_B_bank <= '0;
 			last_issue_dest_PR <= '0;
+			last_issue_ROB_index <= '0;
 
 		    // reg read req to PRF
-			last_PRF_req_A_valid <= 1'b0;
+			last_PRF_req_A_valid <= '0;
 			last_PRF_req_A_PR <= '0;
-			last_PRF_req_B_valid <= 1'b0;
+			last_PRF_req_B_valid <= '0;
 			last_PRF_req_B_PR <= '0;
         end
         else begin
@@ -168,6 +174,7 @@ module alu_iq_wrapper (
 			dispatch_is_imm_by_entry <= next_dispatch_is_imm_by_entry;
 			dispatch_B_ready_by_entry <= next_dispatch_B_ready_by_entry;
 			dispatch_dest_PR_by_entry <= next_dispatch_dest_PR_by_entry;
+			dispatch_ROB_index_by_entry <= next_dispatch_ROB_index_by_entry;
 
 		    // ALU op dispatch feedback by entry
 			last_dispatch_open_by_entry <= dispatch_open_by_entry;
@@ -175,9 +182,9 @@ module alu_iq_wrapper (
 		    // ALU pipeline feedback
 			pipeline_ready <= next_pipeline_ready;
 
-		    // writeback bus
-			WB_valid_by_bank <= next_WB_valid_by_bank;
-			WB_upper_PR_by_bank <= next_WB_upper_PR_by_bank;
+		    // writeback bus by bank
+			WB_bus_valid_by_bank <= next_WB_bus_valid_by_bank;
+			WB_bus_upper_PR_by_bank <= next_WB_bus_upper_PR_by_bank;
 
 		    // ALU op issue to ALU pipeline
 			last_issue_valid <= issue_valid;
@@ -190,6 +197,7 @@ module alu_iq_wrapper (
 			last_issue_B_forward <= issue_B_forward;
 			last_issue_B_bank <= issue_B_bank;
 			last_issue_dest_PR <= issue_dest_PR;
+			last_issue_ROB_index <= issue_ROB_index;
 
 		    // reg read req to PRF
 			last_PRF_req_A_valid <= PRF_req_A_valid;
