7e1a15f3cac1efa108286dae88112ba91fa9641b
Adam Keith <adamkeith@pal-nat186-142-255.itap.purdue.edu>
2025-01-17 12:29:04 -0500

    remove Vivado artifacts

---
+++ uvm/alu_pipeline/testbench/assertions/sva_reset.sv
@@ -0,0 +1,12 @@
+/*
+  Module        : alu_pipeline
+  UMV Component : System Verilog Assertions
+  Author        : Adam Keith
+*/
+
+`ifndef ALU_PIPELINE_RST_SVA_SV
+`define ALU_PIPELINE_RST_SVA_SV
+
+// TODO:
+
+`endif
