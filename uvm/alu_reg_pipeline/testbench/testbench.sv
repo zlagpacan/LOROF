95a8f02c388f894369916e3195b1b43e8d4a753f
Adam Jacob Keith <keithaj@ececomp4.ecn.purdue.edu>
2025-01-28 14:58:14 -0500

    fixed intf DUT instance

--- uvm/alu_reg_pipeline/testbench/testbench.sv
+++ uvm/alu_reg_pipeline/testbench/testbench.sv
@@ -33,6 +33,7 @@ module top;
   alu_reg_pipeline DUT(
     .CLK(CLK),
     .nRST(alu_reg_pipeline_intf.nRST),
+    .issue_valid(alu_reg_pipeline_intf.issue_valid),
     .issue_op(alu_reg_pipeline_intf.issue_op),
     .issue_A_forward(alu_reg_pipeline_intf.issue_A_forward),
     .issue_A_bank(alu_reg_pipeline_intf.issue_A_bank),
@@ -42,7 +43,8 @@ module top;
     .issue_ROB_index(alu_reg_pipeline_intf.issue_ROB_index),
     .A_reg_read_ack(alu_reg_pipeline_intf.A_reg_read_ack),
     .A_reg_read_port(alu_reg_pipeline_intf.A_reg_read_port),
-
+    .B_reg_read_ack(alu_reg_pipeline_intf.B_reg_read_ack),
+    .B_reg_read_port(alu_reg_pipeline_intf.B_reg_read_port),
     .reg_read_data_by_bank_by_port(alu_reg_pipeline_intf.reg_read_data_by_bank_by_port),
     .forward_data_by_bank(alu_reg_pipeline_intf.forward_data_by_bank),
     .WB_ready(alu_reg_pipeline_intf.WB_ready),
