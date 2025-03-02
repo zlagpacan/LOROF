805ee24d3e7c040f3ce4b0c02a3933eb4fcbce94
Adam Keith <adamkeith@pal-nat186-13-99.itap.purdue.edu>
2025-03-02 14:03:40 -0500

    sva

---
+++ uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
@@ -0,0 +1,36 @@
+/*
+  Module        : alu_imm_pipeline
+  UMV Component : System Verilog Assertions
+  Author        : Adam Keith
+*/
+
+`ifndef ALU_IMM_PIPELINE_SVA_SV
+`define ALU_IMM_PIPELINE_SVA_SV
+
+// --- Packages --- //
+`include "core_types_pkg.vh"
+import core_types_pkg::*;
+
+// --- UVM --- //
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+// --- SVA Checks --- //
+module alu_imm_pipeline_sva (
+    input logic                                 CLK,
+    input logic                                 nRST,
+);
+
+   // --- Test Case Coverage --- //
+  sequence DUT_reset;
+    @(posedge CLK) ~nRST;
+  endsequence
+  
+  property DUT_RESET_EVENT;
+    @(posedge CLK) (DUT_reset);
+  endproperty
+  c_ALURP_0: cover property (DUT_RESET_EVENT);
+
+endmodule
+
+`endif
