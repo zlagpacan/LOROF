9361d4c15b97e7fe17adf8be21bf9850a8cc643a
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-07 17:29:01 -0500

    WB stall tc

--- uvm/alu_imm_pipeline/testbench/predictor.sv
+++ uvm/alu_imm_pipeline/testbench/predictor.sv
@@ -1,23 +1,10 @@
-`ifndef ALU_IMM_PIPELINE_PRED_SV
-`define ALU_IMM_PIPELINE_PRED_SV
-
-// --- UVM --- //
-`include "uvm_macros.svh"
-import uvm_pkg::*;
-
-// --- Includes --- //
-`include "sequence_item.sv"
-
-// --- Packages --- //
-`include "core_types_pkg.vh"
-import core_types_pkg::*;
-    
-// --- Predictor --- //
+// Add past values as class members
 class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequence_item); 
     `uvm_component_utils(alu_imm_pipeline_predictor)
 
     uvm_analysis_port#(alu_imm_pipeline_sequence_item) pred_ap;
     alu_imm_pipeline_sequence_item expected_tx;
+    alu_imm_pipeline_sequence_item past_tx; // Store the previous transaction
 
     function new(string name = "alu_imm_pipeline_predictor", uvm_component parent);
         super.new(name, parent);
@@ -26,6 +13,7 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         pred_ap = new("pred_ap", this);
+        past_tx = alu_imm_pipeline_sequence_item::type_id::create("past_tx");
     endfunction : build_phase
 
     function void write(alu_imm_pipeline_sequence_item t);
@@ -33,7 +21,7 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
             expected_tx = alu_imm_pipeline_sequence_item::type_id::create("expected_tx");
         end
         expected_tx.copy(t);
-        
+
         if (t.nRST == 1'b0) begin
             expected_tx.issue_ready  = '1;
             expected_tx.WB_valid     = '0;
@@ -43,18 +31,17 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
         end 
         else begin
             if (t.WB_ready == 1'b0) begin
-                expected_tx.WB_valid     = $past(t.WB_valid);
-                expected_tx.WB_data      = $past(t.WB_data);
-                expected_tx.WB_PR        = $past(t.WB_PR);
-                expected_tx.WB_ROB_index = $past(t.WB_ROB_index);
+                expected_tx.WB_valid     = past_tx.WB_valid;
+                expected_tx.WB_data      = past_tx.WB_data;
+                expected_tx.WB_PR        = past_tx.WB_PR;
+                expected_tx.WB_ROB_index = past_tx.WB_ROB_index;
             end
         end
 
-        // `uvm_info(get_type_name(), $sformatf("Predicted TX: %s", expected_tx.sprint()), UVM_MEDIUM)
+        // Store the current transaction as the past transaction for the next cycle
+        past_tx.copy(t);
 
         pred_ap.write(expected_tx);
     endfunction : write
-    
-endclass
 
-`endif
+endclass
