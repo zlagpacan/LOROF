aa5d9a882a8ac2dd1f165e572bd6ef89f6e459aa
zlagpacan <zlagpacan@gmail.com>
2025-03-08 16:35:56 -0500

    instr_list full enumeration of bits

---
+++ source/rtl/decoder.sv
@@ -0,0 +1,18 @@
+/*
+    Filename: decoder.sv
+    Author: zlagpacan
+    Description: RTL for Decoder
+    Spec: LOROF/spec/design/decoder.md
+*/
+
+`include "core_types_pkg.vh"
+import core_types_pkg::*;
+
+module free_list (
+    
+    // seq
+    input logic CLK,
+    input logic nRST,
+);
+
+endmodule
---
+++ spec/design/isa/csr_list.md
@@ -0,0 +1,2 @@
+# CSR List
+ISA: RV32IMAC_Zicsr_Zifencei Sv32
