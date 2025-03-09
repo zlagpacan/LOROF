7c2e4df1d0b66344fdf4d24996c0c68dab636eae
zlagpacan <zlagpacan@gmail.com>
2025-03-08 22:52:49 -0500

    mdpt update for 8-bit info

--- source/wrappers/mdpt_wrapper.sv
+++ source/wrappers/mdpt_wrapper.sv
@@ -23,13 +23,13 @@ module mdpt_wrapper (
 	input logic [ASID_WIDTH-1:0] next_ASID_REQ,
 
     // RESP stage
-	output logic [MDPT_ENTRIES_PER_BLOCK-1:0] last_dep_pred_by_instr_RESP,
+	output logic [MDPT_ENTRIES_PER_BLOCK-1:0][MDPT_INFO_WIDTH-1:0] last_mdp_info_by_instr_RESP,
 
     // Dep Update 0 stage
 	input logic next_dep_update0_valid,
 	input logic [31:0] next_dep_update0_start_full_PC,
 	input logic [ASID_WIDTH-1:0] next_dep_update0_ASID,
-	input logic next_dep_update0_dep_truth
+	input logic [MDPT_INFO_WIDTH-1:0] next_dep_update0_mdp_info
 );
 
     // ----------------------------------------------------------------
@@ -42,13 +42,13 @@ module mdpt_wrapper (
 	logic [ASID_WIDTH-1:0] ASID_REQ;
 
     // RESP stage
-	logic [MDPT_ENTRIES_PER_BLOCK-1:0] dep_pred_by_instr_RESP;
+	logic [MDPT_ENTRIES_PER_BLOCK-1:0][MDPT_INFO_WIDTH-1:0] mdp_info_by_instr_RESP;
 
     // Dep Update 0 stage
 	logic dep_update0_valid;
 	logic [31:0] dep_update0_start_full_PC;
 	logic [ASID_WIDTH-1:0] dep_update0_ASID;
-	logic dep_update0_dep_truth;
+	logic [MDPT_INFO_WIDTH-1:0] dep_update0_mdp_info;
 
     // ----------------------------------------------------------------
     // Module Instantiation:
@@ -68,13 +68,13 @@ module mdpt_wrapper (
 			ASID_REQ <= '0;
 
 		    // RESP stage
-			last_dep_pred_by_instr_RESP <= '0;
+			last_mdp_info_by_instr_RESP <= '0;
 
 		    // Dep Update 0 stage
 			dep_update0_valid <= '0;
 			dep_update0_start_full_PC <= '0;
 			dep_update0_ASID <= '0;
-			dep_update0_dep_truth <= '0;
+			dep_update0_mdp_info <= '0;
         end
         else begin
 
@@ -85,13 +85,13 @@ module mdpt_wrapper (
 			ASID_REQ <= next_ASID_REQ;
 
 		    // RESP stage
-			last_dep_pred_by_instr_RESP <= dep_pred_by_instr_RESP;
+			last_mdp_info_by_instr_RESP <= mdp_info_by_instr_RESP;
 
 		    // Dep Update 0 stage
 			dep_update0_valid <= next_dep_update0_valid;
 			dep_update0_start_full_PC <= next_dep_update0_start_full_PC;
 			dep_update0_ASID <= next_dep_update0_ASID;
-			dep_update0_dep_truth <= next_dep_update0_dep_truth;
+			dep_update0_mdp_info <= next_dep_update0_mdp_info;
         end
     end
 
