3a88ac5a6c682472eaf0b9f2b7ed057ca8e6eeb1
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-12 20:33:39 -0400

    standard wb tc

--- uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
+++ uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
@@ -68,9 +68,21 @@ module alu_imm_pipeline_sva (
   logic inv_WB_ready;
   logic inv_issue_valid;
 
-  assign inv_nRST        = $past(nRST, 1) | $past(nRST, 2) | $past(nRST, 3);
-  assign inv_WB_ready    = $past(WB_ready, 1) | $past(WB_ready, 2) | $past(WB_ready, 3);
-  assign inv_issue_valid = $past(issue_valid, 1) | $past(issue_valid, 2) | $past(issue_valid, 3);
+  // assign inv_nRST        = $past(nRST, 1) | $past(nRST, 2) | $past(nRST, 3);
+  // assign inv_WB_ready    = $past(WB_ready, 1) | $past(WB_ready, 2) | $past(WB_ready, 3);
+  // assign inv_issue_valid = $past(issue_valid, 1) | $past(issue_valid, 2) | $past(issue_valid, 3);
+
+  always_ff @(posedge CLK or negedge nRST) begin
+    if (~nRST) begin
+        inv_nRST        <= 1'b0;
+        inv_WB_ready    <= 1'b0;
+        inv_issue_valid <= 1'b0;
+    end else begin
+        inv_nRST        <= $past(nRST, 1) | $past(nRST, 2) | $past(nRST, 3);
+        inv_WB_ready    <= $past(WB_ready, 1) | $past(WB_ready, 2) | $past(WB_ready, 3);
+        inv_issue_valid <= $past(issue_valid, 1) | $past(issue_valid, 2) | $past(issue_valid, 3);
+    end
+  end
 
   property tc_standard_WB_PR;
     @(posedge CLK) disable iff (~inv_nRST || ~inv_WB_ready || ~inv_issue_valid)
