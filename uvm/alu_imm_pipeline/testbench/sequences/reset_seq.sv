c3aff0e7f8b63730c248f633a12448b2b790288c
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-12 20:43:41 -0400

    standard wb tc

--- uvm/alu_imm_pipeline/testbench/sequences/reset_seq.sv
+++ uvm/alu_imm_pipeline/testbench/sequences/reset_seq.sv
@@ -40,31 +40,3 @@ class reset_sequence extends uvm_sequence;
   endtask : body
   
 endclass : reset_sequence
-
-// --- Garbage Sequence - to prime reset --- //
-class garbage_sequence extends uvm_sequence;
-  `uvm_object_utils(garbage_sequence)
-  
-  alu_imm_pipeline_sequence_item garbage_pkt;
-  
-  // --- Constructor --- //
-  function new(string name= "garbage_sequence");
-    super.new(name);
-    `uvm_info("GARBAGE_SEQ", "Inside Constructor", UVM_HIGH)
-  endfunction
-  
-  // --- Body Task --- //
-  task body();
-    `uvm_info("GARBAGE_SEQ", "Inside body task", UVM_HIGH)
-    
-    // --- Randomize With Reset --- //
-    garbage_pkt = alu_imm_pipeline_sequence_item::type_id::create("garbage_pkt");
-    start_item(garbage_pkt);
-    garbage_pkt.randomize() with {nRST==1;};
-    finish_item(garbage_pkt);
-        
-  endtask : body
-  
-endclass : garbage_sequence
-
-`endif
