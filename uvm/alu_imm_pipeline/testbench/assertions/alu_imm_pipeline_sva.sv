61b598a92603e94c19c0f408b9b77dd9922f945a
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-13 10:40:25 -0400

    standard wb tc

--- uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
+++ uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
@@ -74,13 +74,13 @@ module alu_imm_pipeline_sva (
 
   always_ff @(posedge CLK or negedge nRST) begin
     if (~nRST) begin
-        inv_nRST        <= 1'b0;
-        inv_WB_ready    <= 1'b0;
-        inv_issue_valid <= 1'b0;
+      inv_nRST        <= 1'b0;
+      inv_WB_ready    <= 1'b0;
+      inv_issue_valid <= 1'b0;
     end else begin
-        inv_nRST        <= $past(nRST, 1) || $past(nRST, 2) || $past(nRST, 3);
-        inv_WB_ready    <= $past(WB_ready, 1) || $past(WB_ready, 2) || $past(WB_ready, 3);
-        inv_issue_valid <= $past(issue_valid, 1) || $past(issue_valid, 2) || $past(issue_valid, 3);
+      inv_nRST        <= ($past(nRST, 1) == 1) || ($past(nRST, 2) == 1) || ($past(nRST, 3) == 1);
+      inv_WB_ready    <= ($past(WB_ready, 1) == 1) || ($past(WB_ready, 2) == 1) || ($past(WB_ready, 3) == 1);
+      inv_issue_valid <= ($past(issue_valid, 1) == 1) || ($past(issue_valid, 2) == 1) || ($past(issue_valid, 3) == 1);
     end
   end
 
