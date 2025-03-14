9100d6fc7dd6cc5b8b5daa7073ed775a09626ca7
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-14 17:24:07 -0400

    test sva property

--- uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
+++ uvm/alu_imm_pipeline/testbench/assertions/alu_imm_pipeline_sva.sv
@@ -93,7 +93,7 @@ module alu_imm_pipeline_sva (
   // TODO: test one to see how we like
   property tc_standard_WB_data_ORI;
     @(posedge CLK) disable iff (inv_nRST || inv_WB_ready || inv_issue_valid || ~nRST)
-    (WB_valid) |-> (WB_data == ({{20{$past(issue_imm12[11], 3)}}, $past(issue_imm12, 3)}) || ($past(forward_data_by_bank[$past(issue_A_bank, 3)], 2)));
+    (WB_valid) |-> (WB_data == ({{20{$past(issue_imm12[11], 3)}}, $past(issue_imm12, 3)}) | ($past(forward_data_by_bank[$past(issue_A_bank, 3)], 2)));
   endproperty
 
   a_tc_standard_WB_data_ORI: assert property (tc_standard_WB_data_ORI) begin
--- uvm/alu_imm_pipeline/testbench/predictor.sv
+++ uvm/alu_imm_pipeline/testbench/predictor.sv
@@ -74,9 +74,9 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
 
             // FIXME: going once cycle too early
             if (t.WB_ready) begin
-                stage1_A_bank = t.forward_data_by_bank[t.issue_A_bank];
-                stage1_imm    = {{20{t.issue_imm12[11]}}, t.issue_imm12};
-                stage1_op     = t.issue_op;
+                stage1_A_bank <= t.forward_data_by_bank[t.issue_A_bank];
+                stage1_imm    <= {{20{t.issue_imm12[11]}}, t.issue_imm12};
+                stage1_op     <= t.issue_op;
 
                 expected_tx.WB_data = stage1_A_bank | stage1_imm;
             end else begin
