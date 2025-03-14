e505205227ee83ee1a5c1b32710916f20d11cfd9
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-13 21:10:23 -0400

    alu_imm pred

--- uvm/alu_imm_pipeline/testbench/sequence_item.sv
+++ uvm/alu_imm_pipeline/testbench/sequence_item.sv
@@ -79,6 +79,9 @@ class alu_imm_pipeline_sequence_item extends uvm_sequence_item;
   constraint issue_A_forward_ideal { soft issue_A_forward == 1'b1; }
   constraint issue_A_is_zero_ideal { soft issue_A_is_zero == 1'b0; }
 
+  constraint TEST { soft issue_op == '0; }
+
+
 
   // Temp Void - Dist constraints won't override
   // constraint WB_ready_ideal        { soft WB_ready        == 1'b1; }
