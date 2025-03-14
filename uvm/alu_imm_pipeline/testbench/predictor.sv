1752baa135ffaf7661dd870b9cf47c1a59dc5776
Adam Keith <adamkeith@Adams-MacBook-Pro.local>
2025-03-13 21:52:13 -0400

    fix pred

--- uvm/alu_imm_pipeline/testbench/predictor.sv
+++ uvm/alu_imm_pipeline/testbench/predictor.sv
@@ -64,7 +64,7 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
         end 
         else begin
             // stage1_A_bank <= t.issue_A_bank;
-            stage1_A   <= t.forward_data_by_bank[stage1_A_bank];
+            stage1_A_bank   <= t.forward_data_by_bank[t.issue_A_bank];
             stage1_imm <= {{20{t.issue_imm12[11]}}, t.issue_imm12};
             stage1_op  <= t.issue_op;
 
@@ -73,7 +73,7 @@ class alu_imm_pipeline_predictor extends uvm_subscriber#(alu_imm_pipeline_sequen
             // stage2_op  <= stage1_op;
 
             // FIXME: going once cycle too early
-            expected_tx.WB_data = stage1_A | stage1_imm;
+            expected_tx.WB_data = stage1_A_bank | stage1_imm;
 
             // stage3_A   <= stage2_A;
             // stage3_imm <= stage2_imm;
