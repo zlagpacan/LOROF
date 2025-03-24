71179258bb9cbbe67ca409c3a205096a30618109
zlagpacan <zlagpacan@gmail.com>
2025-03-24 01:14:56 -0400

    ar_dep_check done

--- tb/ar_dep_check_tb.sv
+++ tb/ar_dep_check_tb.sv
@@ -214,38 +214,38 @@ module ar_dep_check_tb ();
 		check_outputs();
 
         // ------------------------------------------------------------
-        // default:
-        test_case = "default";
+        // simple coverage:
+        test_case = "simple coverage";
         $display("\ntest %0d: %s", test_num, test_case);
         test_num++;
 
 		@(posedge CLK); #(PERIOD/10);
 
 		// inputs
-		sub_test_case = "default";
+		sub_test_case = "no matches";
 		$display("\t- sub_test: %s", sub_test_case);
 
 		// reset
 		nRST = 1'b1;
 	    // inputs by way
-		tb_regwrite_by_way = 4'b0000;
+		tb_regwrite_by_way = 4'b1111;
 		tb_A_AR_by_way = {
 			5'h0,
-			5'h0,
-			5'h0,
-			5'h0
+			5'h1,
+			5'h2,
+			5'h3
 		};
 		tb_B_AR_by_way = {
-			5'h0,
-			5'h0,
-			5'h0,
-			5'h0
+			5'h4,
+			5'h5,
+			5'h6,
+			5'h7
 		};
 		tb_dest_AR_by_way = {
-			5'h0,
-			5'h0,
-			5'h0,
-			5'h0
+			5'h8,
+			5'h9,
+			5'ha,
+			5'hb
 		};
 	    // outputs by way
 
@@ -265,6 +265,377 @@ module ar_dep_check_tb ();
 		expected_B_PR_dep_by_way = 4'b0000;
 		expected_B_PR_sel_by_way = {
 			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "matches way 0";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b0001;
+		tb_A_AR_by_way = {
+			5'hc,
+			5'h3,
+			5'hc,
+			5'hc
+		};
+		tb_B_AR_by_way = {
+			5'hc,
+			5'hc,
+			5'h5,
+			5'h4
+		};
+		tb_dest_AR_by_way = {
+			5'h3,
+			5'h4,
+			5'h5,
+			5'hc
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b1010;
+		expected_A_PR_sel_by_way = {
+			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b1100;
+		expected_B_PR_sel_by_way = {
+			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "matches way 1";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b0110;
+		tb_A_AR_by_way = {
+			5'h10,
+			5'h1d,
+			5'h1d,
+			5'hc
+		};
+		tb_B_AR_by_way = {
+			5'h1d,
+			5'h11,
+			5'h12,
+			5'h1d
+		};
+		tb_dest_AR_by_way = {
+			5'h11,
+			5'h4,
+			5'h1d,
+			5'h12
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b0100;
+		expected_A_PR_sel_by_way = {
+			2'h0,
+			2'h1,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b1000;
+		expected_B_PR_sel_by_way = {
+			2'h1,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "matches way 2";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b0101;
+		tb_A_AR_by_way = {
+			5'h12,
+			5'h12,
+			5'h6,
+			5'h12
+		};
+		tb_B_AR_by_way = {
+			5'h6,
+			5'h12,
+			5'h8,
+			5'h0
+		};
+		tb_dest_AR_by_way = {
+			5'h6,
+			5'h12,
+			5'h0,
+			5'h9
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b1000;
+		expected_A_PR_sel_by_way = {
+			2'h2,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b0000;
+		expected_B_PR_sel_by_way = {
+			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "matches way 3";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b1001;
+		tb_A_AR_by_way = {
+			5'h3,
+			5'h2,
+			5'h3,
+			5'h2
+		};
+		tb_B_AR_by_way = {
+			5'h2,
+			5'h3,
+			5'h2,
+			5'h3
+		};
+		tb_dest_AR_by_way = {
+			5'h3,
+			5'h7,
+			5'h6,
+			5'h5
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b0000;
+		expected_A_PR_sel_by_way = {
+			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b0000;
+		expected_B_PR_sel_by_way = {
+			2'h0,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "bunch of crap 0";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b1011;
+		tb_A_AR_by_way = {
+			5'h4,
+			5'h3,
+			5'h2,
+			5'h1
+		};
+		tb_B_AR_by_way = {
+			5'h2,
+			5'h3,
+			5'h4,
+			5'h5
+		};
+		tb_dest_AR_by_way = {
+			5'h5,
+			5'h4,
+			5'h3,
+			5'h2
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b0110;
+		expected_A_PR_sel_by_way = {
+			2'h0,
+			2'h1,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b1100;
+		expected_B_PR_sel_by_way = {
+			2'h0,
+			2'h1,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "bunch of crap 1";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b1011;
+		tb_A_AR_by_way = {
+			5'h2,
+			5'h2,
+			5'h4,
+			5'h5
+		};
+		tb_B_AR_by_way = {
+			5'h2,
+			5'h3,
+			5'h2,
+			5'h5
+		};
+		tb_dest_AR_by_way = {
+			5'h5,
+			5'h4,
+			5'h2,
+			5'h2
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b1100;
+		expected_A_PR_sel_by_way = {
+			2'h1,
+			2'h1,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b1010;
+		expected_B_PR_sel_by_way = {
+			2'h1,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+
+		check_outputs();
+
+		@(posedge CLK); #(PERIOD/10);
+
+		// inputs
+		sub_test_case = "bunch of crap 2";
+		$display("\t- sub_test: %s", sub_test_case);
+
+		// reset
+		nRST = 1'b1;
+	    // inputs by way
+		tb_regwrite_by_way = 4'b1101;
+		tb_A_AR_by_way = {
+			5'h9,
+			5'h9,
+			5'h9,
+			5'h9
+		};
+		tb_B_AR_by_way = {
+			5'h9,
+			5'h9,
+			5'h9,
+			5'h9
+		};
+		tb_dest_AR_by_way = {
+			5'h9,
+			5'h9,
+			5'h9,
+			5'h9
+		};
+	    // outputs by way
+
+		@(negedge CLK);
+
+		// outputs:
+
+	    // inputs by way
+	    // outputs by way
+		expected_A_PR_dep_by_way = 4'b1110;
+		expected_A_PR_sel_by_way = {
+			2'h2,
+			2'h0,
+			2'h0,
+			2'h0
+		};
+		expected_B_PR_dep_by_way = 4'b1110;
+		expected_B_PR_sel_by_way = {
+			2'h2,
 			2'h0,
 			2'h0,
 			2'h0
