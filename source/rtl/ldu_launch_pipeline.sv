f8b1638fa702b777d9bb856ebee2ee1eb9a85f8f
zlagpacan <zlagpacan@gmail.com>
2025-06-07 02:05:52 -0400

    RTL v1 done

--- source/rtl/ldu_launch_pipeline.sv
+++ source/rtl/ldu_launch_pipeline.sv
@@ -120,6 +120,9 @@ module ldu_launch_pipeline #(
     output logic [LOG_PR_COUNT-1:0]     WB_PR,
     output logic [LOG_ROB_ENTRIES-1:0]  WB_ROB_index,
 
+    // writeback backpressure from PRF
+    input logic                         WB_ready,
+
     // CAM launch
     output logic                            stamofu_CAM_launch_valid,
     output logic [PA_WIDTH-2-1:0]           stamofu_CAM_launch_PA_word,
@@ -143,7 +146,6 @@ module ldu_launch_pipeline #(
     output logic                            ldu_cq_info_ret_dcache_hit,
     output logic                            ldu_cq_info_ret_is_mem,
     output logic                            ldu_cq_info_ret_aq_blocking,
-    output logic                            ldu_cq_info_ret_mdp_present,
     output logic [31:0]                     ldu_cq_info_ret_data,
 
     // misaligned queue info ret
@@ -153,7 +155,6 @@ module ldu_launch_pipeline #(
     output logic                            ldu_mq_info_ret_dcache_hit,
     output logic                            ldu_mq_info_ret_is_mem,
     output logic                            ldu_mq_info_ret_aq_blocking,
-    output logic                            ldu_mq_info_ret_mdp_present,
     output logic [31:0]                     ldu_mq_info_ret_data,
 
     // misprediction notification to ROB
@@ -247,6 +248,7 @@ module ldu_launch_pipeline #(
     logic [DCACHE_TAG_WIDTH-1:0]    RESP_stage_dcache_tag;
     logic [1:0]                     RESP_stage_dcache_vtm_by_way;
     logic                           RESP_stage_dcache_vtm;
+    logic                           RESP_stage_dcache_hit;
 
     logic                           RESP_stage_do_WB;
     logic                           RESP_stage_do_CAM;
@@ -265,11 +267,12 @@ module ldu_launch_pipeline #(
     logic                           RET_stage_is_mq;
     logic                           RET_stage_misaligned;
     logic [3:0]                     RET_stage_op;
-    logic                           RET_stage_mdp_present;
     logic [LOG_PR_COUNT-1:0]        RET_stage_dest_PR;
     logic [LOG_ROB_ENTRIES-1:0]     RET_stage_ROB_index;
     logic                           RET_stage_page_fault;
     logic                           RET_stage_access_fault;
+    logic                           RET_stage_dtlb_hit;
+    logic                           RET_stage_dcache_hit;
     logic                           RET_stage_is_mem;
     logic [PPN_WIDTH-1:0]           RET_stage_PA_word;
     logic                           RET_stage_aq_blocking;
@@ -382,7 +385,7 @@ module ldu_launch_pipeline #(
         REQ_stage_byte_mask = first_try_ack ? first_try_byte_mask : second_try_byte_mask;
         REQ_stage_given_data = data_try_data;
         REQ_stage_cq_index = first_try_ack ? first_try_cq_index : second_try_ack ? second_try_cq_index : data_try_cq_index;
-        REQ_stage_mq_index = first_try_ack ? ldu_mq_enq_index : second_try_cq_index;
+        REQ_stage_mq_index = first_try_ack ? ldu_mq_enq_index : second_try_mq_index;
 
         ldu_mq_enq_valid = first_try_ack & first_try_is_mq;
 
@@ -495,18 +498,17 @@ module ldu_launch_pipeline #(
         RESP_stage_dcache_vtm_by_way[0] = dcache_resp_valid_by_way[0] & (dcache_resp_tag_by_way[0] == RESP_stage_dcache_tag);
         RESP_stage_dcache_vtm_by_way[1] = dcache_resp_valid_by_way[1] & (dcache_resp_tag_by_way[1] == RESP_stage_dcache_tag);
         RESP_stage_dcache_vtm = |RESP_stage_dcache_vtm_by_way;
+        RESP_stage_dcache_hit = RESP_stage_dtlb_hit & RESP_stage_dcache_vtm;
 
         dcache_resp_hit_valid = 
             RESP_stage_valid
             & ~stall_RET
-            & (RESP_stage_is_first | RESP_stage_is_second)
             & RESP_stage_dtlb_hit
             & RESP_stage_dcache_vtm;
         dcache_resp_hit_way = RESP_stage_dcache_vtm_by_way[1];
         dcache_resp_miss_valid = 
             RESP_stage_valid
             & ~stall_RET
-            & (RESP_stage_is_first | RESP_stage_is_second)
             & RESP_stage_dtlb_hit
             & ~RESP_stage_dcache_vtm;
         dcache_resp_miss_tag = RESP_stage_dcache_tag;
@@ -538,11 +540,12 @@ module ldu_launch_pipeline #(
             RET_stage_is_mq <= '0;
             RET_stage_misaligned <= '0;
             RET_stage_op <= '0;
-            RET_stage_mdp_present <= '0;
             RET_stage_dest_PR <= '0;
             RET_stage_ROB_index <= '0;
             RET_stage_page_fault <= '0;
             RET_stage_access_fault <= '0;
+            RET_stage_dtlb_hit <= '0;
+            RET_stage_dcache_hit <= '0;
             RET_stage_is_mem <= '0;
             RET_stage_PA_word <= '0;
             RET_stage_aq_blocking <= '0;
@@ -565,31 +568,170 @@ module ldu_launch_pipeline #(
             RET_stage_is_mq <= RESP_stage_is_mq;
             RET_stage_misaligned <= RESP_stage_misaligned;
             RET_stage_op <= RESP_stage_op;
-            RET_stage_mdp_present <= RESP_stage_mdp_present;
             RET_stage_dest_PR <= RESP_stage_dest_PR;
             RET_stage_ROB_index <= RESP_stage_ROB_index;
             RET_stage_page_fault <= RESP_stage_selected_page_fault;
             RET_stage_access_fault <= RESP_stage_selected_access_fault;
+            RET_stage_dtlb_hit <= RESP_stage_dtlb_hit;
+            RET_stage_dcache_hit <= RESP_stage_dcache_hit;
             RET_stage_is_mem <= RESP_stage_selected_is_mem;
             RET_stage_PA_word <= RESP_stage_selected_PA_word;
             RET_stage_aq_blocking <= RESP_stage_aq_blocking;
             RET_stage_byte_mask <= RESP_stage_byte_mask;
             RET_stage_data <= RESP_stage_selected_data;
-            RET_stage_cq_index <= RET_stage_cq_index;
-            RET_stage_mq_index <= RET_stage_mq_index;
-            RET_stage_do_WB <= RET_stage_do_WB;
-            RET_stage_do_CAM <= RET_stage_do_CAM;
-            RET_stage_do_exception <= RET_stage_do_exception;
-            RET_stage_do_mispred <= RET_stage_do_mispred;
-            RET_stage_do_cq_ret <= RET_stage_do_cq_ret;
-            RET_stage_do_mq_ret <= RET_stage_do_mq_ret;
+            RET_stage_cq_index <= RESP_stage_cq_index;
+            RET_stage_mq_index <= RESP_stage_mq_index;
+            RET_stage_do_WB <= RESP_stage_do_WB;
+            RET_stage_do_CAM <= RESP_stage_do_CAM;
+            RET_stage_do_exception <= RESP_stage_do_exception;
+            RET_stage_do_mispred <= RESP_stage_do_mispred;
+            RET_stage_do_cq_ret <= RESP_stage_do_cq_ret;
+            RET_stage_do_mq_ret <= RESP_stage_do_mq_ret;
         end
     end
 
     // stall and control logic
     always_comb begin
 
-        
+        // check valid RET
+        if (RET_stage_valid) begin
+
+            // check any conditions for stall
+                // do's not satisfied
+            if (
+                RET_stage_do_WB & ~WB_ready
+                // CAM always ready
+                | RET_stage_do_exception & ~rob_exception_ready
+                | RET_stage_do_mispred & ~mispred_notif_ready
+                // cq ret always ready
+                // mq ret always ready
+            ) begin
+                RET_stage_perform = 1'b0;
+                stall_RET = 1'b1;
+            end
+
+            // otherwise, good to perform RET
+            else begin
+                RET_stage_perform = 1'b1;
+                stall_RET = 1'b0;
+            end
+        end
+
+        // otherwise, NOP
+        else begin
+            RET_stage_perform = 1'b0;
+            stall_RET = 1'b0;
+        end
+    end
+
+    // dataflow
+    always_comb begin
+
+        // WB
+        WB_valid = RET_stage_do_WB & RET_stage_perform;
+        WB_PR = RET_stage_dest_PR;
+        WB_ROB_index = RET_stage_ROB_index;
+
+        // WB data
+            // need to mux around return data bits for non-word-alignment
+        if (RET_stage_is_data) begin
+            // take data as-is
+                // muxing around was already performed as needed
+            WB_data = RET_stage_data;
+        end
+        else begin
+            // op-wise and byte-mask-wise muxing
+            // can assume not misaligned at this point
+            // sign extension:
+                // signed & msb -> ~op[2] & msb
+
+            // LW
+            if (RET_stage_op[1]) begin
+
+                // guaranteed 4'b1111 -> bits [31:0]
+                WB_data = RET_stage_data;
+            end
+
+            // LH, LHU
+            else if (RET_stage_op[0]) begin
+
+                // 4'b1100 -> bits [31:16]
+                if (~RET_stage_byte_mask[1]) begin
+                    WB_data = {{16{~RET_stage_op[2] & RET_stage_data[31]}}, RET_stage_data[31:16]};
+                end
+
+                // 4'b0110 -> bits [23:8]
+                else if (~RET_stage_byte_mask[0]) begin
+                    WB_data = {{16{~RET_stage_op[2] & RET_stage_data[23]}}, RET_stage_data[23:8]};
+                end
+
+                // 4'b0011 -> bits [15:0]
+                else begin
+                    WB_data = {{16{~RET_stage_op[2] & RET_stage_data[15]}}, RET_stage_data[15:0]};
+                end
+            end
+
+            // LB, LBU
+            else begin
+
+                // 4'b1000 -> bits [31:24]
+                if (RET_stage_byte_mask[3]) begin
+                    WB_data = {{24{~RET_stage_op[2] & RET_stage_data[31]}}, RET_stage_data[31:24]};
+                end
+
+                // 4'b0100 -> bits [23:16]
+                else if (RET_stage_byte_mask[2]) begin
+                    WB_data = {{24{~RET_stage_op[2] & RET_stage_data[23]}}, RET_stage_data[23:16]};
+                end
+
+                // 4'b0010 -> bits [15:8]
+                else if (RET_stage_byte_mask[1]) begin
+                    WB_data = {{24{~RET_stage_op[2] & RET_stage_data[15]}}, RET_stage_data[15:8]};
+                end
+
+                // 4'b0001 -> bits [15:8]
+                else begin
+                    WB_data = {{24{~RET_stage_op[2] & RET_stage_data[7]}}, RET_stage_data[7:0]};
+                end
+            end
+        end
+
+        // CAM
+        stamofu_CAM_launch_valid = RET_stage_do_CAM & RET_stage_perform;
+        stamofu_CAM_launch_PA_word = RET_stage_PA_word;
+        stamofu_CAM_launch_byte_mask = RET_stage_byte_mask;
+        stamofu_CAM_launch_ROB_index = RET_stage_ROB_index;
+        stamofu_CAM_launch_cq_index = RET_stage_cq_index;
+        stamofu_CAM_launch_is_mq = RET_stage_is_mq;
+        stamofu_CAM_launch_mq_index = RET_stage_mq_index;
+
+        // exception
+        rob_exception_valid = RET_stage_do_exception & RET_stage_perform;
+        rob_exception_page_fault = RET_stage_page_fault;
+        rob_exception_access_fault = RET_stage_access_fault;
+        rob_exception_ROB_index = RET_stage_ROB_index;
+
+        // mispred
+        mispred_notif_valid = RET_stage_do_mispred & RET_stage_perform;
+        mispred_notif_ROB_index = RET_stage_ROB_index;
+
+        // cq ret
+        ldu_cq_info_ret_valid = RET_stage_do_cq_ret & RET_stage_perform;
+        ldu_cq_info_ret_cq_index = RET_stage_cq_index;
+        ldu_cq_info_ret_dtlb_hit = RET_stage_dtlb_hit;
+        ldu_cq_info_ret_dcache_hit = RET_stage_dcache_hit;
+        ldu_cq_info_ret_is_mem = RET_stage_is_mem;
+        ldu_cq_info_ret_aq_blocking = RET_stage_aq_blocking;
+        ldu_cq_info_ret_data = RET_stage_data;
+
+        // mq ret
+        ldu_mq_info_ret_valid = RET_stage_do_mq_ret & RET_stage_perform;
+        ldu_mq_info_ret_mq_index = RET_stage_mq_index;
+        ldu_mq_info_ret_dtlb_hit = RET_stage_dtlb_hit;
+        ldu_mq_info_ret_dcache_hit = RET_stage_dcache_hit;
+        ldu_mq_info_ret_is_mem = RET_stage_is_mem;
+        ldu_mq_info_ret_aq_blocking = RET_stage_aq_blocking;
+        ldu_mq_info_ret_data = RET_stage_data;
     end
 
 endmodule
