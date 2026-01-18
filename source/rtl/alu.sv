2bcea98919217d4c904cc6ccd5a2e1e6ef9008e2
zlagpacan <zlagpacan@gmail.com>
2026-01-18 04:05:42 -0500

    alu spacing alignment

--- source/rtl/alu.sv
+++ source/rtl/alu.sv
@@ -17,37 +17,37 @@ module alu (
 
     always_comb begin
         unique casez (op)
-            instrp::ALU_ADD:   out = A + B;
-            instrp::ALU_SLL:   out = A << B[5:0];
-            instrp::ALU_SLT:   out = $signed(A) < $signed(B) ? 64'h1 : 64'h0;
-            instrp::ALU_SLTU:  out = A < B ? 64'h1 : 64'h0;
-            instrp::ALU_XOR:   out = A ^ B;
-            instrp::ALU_SRL:   out = A >> B[5:0];
-            instrp::ALU_OR:    out = A | B;
-            instrp::ALU_AND:   out = A & B;
-            instrp::ALU_SUB:   out = A - B;
-            instrp::ALU_SRA:   out = $signed(A) >>> B[5:0];
+            instrp::ALU_ADD:    out = A + B;
+            instrp::ALU_SLL:    out = A << B[5:0];
+            instrp::ALU_SLT:    out = $signed(A) < $signed(B) ? 64'h1 : 64'h0;
+            instrp::ALU_SLTU:   out = A < B ? 64'h1 : 64'h0;
+            instrp::ALU_XOR:    out = A ^ B;
+            instrp::ALU_SRL:    out = A >> B[5:0];
+            instrp::ALU_OR:     out = A | B;
+            instrp::ALU_AND:    out = A & B;
+            instrp::ALU_SUB:    out = A - B;
+            instrp::ALU_SRA:    out = $signed(A) >>> B[5:0];
             instrp::ALU_ADDW: begin
-                out[31:0] = A[31:0] + B[31:0];
-                out[63:32] = {32{out[31]}};
+                                out[31:0] = A[31:0] + B[31:0];
+                                out[63:32] = {32{out[31]}};
             end
             instrp::ALU_SUBW: begin
-                out[31:0] = A[31:0] - B[31:0];
-                out[63:32] = {32{out[31]}};
+                                out[31:0] = A[31:0] - B[31:0];
+                                out[63:32] = {32{out[31]}};
             end
             instrp::ALU_SLLW: begin
-                out[31:0] = A[31:0] << B[4:0];
-                out[63:32] = {32{out[31]}};
+                                out[31:0] = A[31:0] << B[4:0];
+                                out[63:32] = {32{out[31]}};
             end
             instrp::ALU_SRLW: begin
-                out[31:0] = A[31:0] >> B[4:0];
-                out[63:32] = {32{out[31]}};
+                                out[31:0] = A[31:0] >> B[4:0];
+                                out[63:32] = {32{out[31]}};
             end
             instrp::ALU_SRAW: begin
-                out[31:0] = $signed(A[31:0]) >>> B[4:0];
-                out[63:32] = {32{out[31]}};
+                                out[31:0] = $signed(A[31:0]) >>> B[4:0];
+                                out[63:32] = {32{out[31]}};
             end
-            default:                out = A + B;
+            default:            out = A + B;
         endcase
     end
 
