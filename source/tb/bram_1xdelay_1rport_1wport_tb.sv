700cd97a3eb12c9b46fd5c8f0ec2075e902c83d9
zlagpacan <zlagpacan@gmail.com>
2026-02-22 00:14:43 -0500

    tb done

--- source/tb/bram_1xdelay_1rport_1wport_tb.sv
+++ source/tb/bram_1xdelay_1rport_1wport_tb.sv
@@ -136,33 +136,302 @@ module bram_1xdelay_1rport_1wport_tb #(
 		check_outputs();
 
         // ------------------------------------------------------------
-        // default:
-        test_case = "default";
+        // update chain:
+        test_case = "update chain";
         $display("\ntest %0d: %s", test_num, test_case);
         test_num++;
 
-		@(posedge CLK); #(PERIOD/10);
+        for (int i = 0; i < 32; i++) begin
 
-		// inputs
-		sub_test_case = "default";
-		$display("\t- sub_test: %s", sub_test_case);
+            @(posedge CLK); #(PERIOD/10);
 
-		// reset
-		nRST = 1'b1;
-		tb_ren0 = '0;
-		tb_ren1 = '0;
-		tb_rindex = '0;
-		tb_wen_byte = '0;
-		tb_windex = '0;
-		tb_wdata = '0;
+            // inputs
+            sub_test_case = {
+                "\n\t\tupdate:  ", $sformatf("%02h", i),
+                "\n\t\tREQ:     i",
+                "\n\t\tRESP0:   i",
+                "\n\t\tRESP1:   i"
+            };
+            $display("\t- sub_test: %s", sub_test_case);
 
-		@(negedge CLK);
+            // reset
+            nRST = 1'b1;
+            tb_ren0 = 1'b0;
+            tb_ren1 = 1'b0;
+            tb_rindex = 5'h00;
+            tb_wen_byte = 4'b1111;
+            tb_windex = i[4:0];
+            tb_wdata = {7{i[4:0]}};
 
-		// outputs:
+            @(negedge CLK);
 
-		expected_rdata = '0;
+            // outputs:
 
-		check_outputs();
+            expected_rdata = 32'h00000000;
+
+            check_outputs();
+        end
+
+        // ------------------------------------------------------------
+        // read chain:
+        test_case = "read chain";
+        $display("\ntest %0d: %s", test_num, test_case);
+        test_num++;
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     0",
+            "\n\t\tRESP0:   i",
+            "\n\t\tRESP1:   i"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h00;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = 32'h00000000;
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     1",
+            "\n\t\tRESP0:   0",
+            "\n\t\tRESP1:   i"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h01;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = 32'h00000000;
+
+        check_outputs();
+
+        for (int i = 0; i < 29; i++) begin
+
+            @(posedge CLK); #(PERIOD/10);
+
+            // inputs
+            sub_test_case = {
+                "\n\t\tupdate:  i",
+                "\n\t\tREQ:     ", $sformatf("%02h", {i+2}[4:0]),
+                "\n\t\tRESP0:   ", $sformatf("%02h", {i+1}[4:0]),
+                "\n\t\tRESP1:   ", $sformatf("%02h", i[4:0])
+            };
+            $display("\t- sub_test: %s", sub_test_case);
+
+            // reset
+            nRST = 1'b1;
+            tb_ren0 = 1'b1;
+            tb_ren1 = 1'b1;
+            tb_rindex = {i+2}[4:0];
+            tb_wen_byte = 4'b0000;
+            tb_windex = 5'h00;
+            tb_wdata = 32'h00000000;
+
+            @(negedge CLK);
+
+            // outputs:
+
+            expected_rdata = {7{i[4:0]}};
+
+            check_outputs();
+        end
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     stall",
+            "\n\t\tRESP0:   1e",
+            "\n\t\tRESP1:   1d"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b0;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h1f;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h1d}}};
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     1f",
+            "\n\t\tRESP0:   1e",
+            "\n\t\tRESP1:   1e"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h1f;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h1e}}};
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     00",
+            "\n\t\tRESP0:   1f",
+            "\n\t\tRESP1:   1e"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h00;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h1e}}};
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     01",
+            "\n\t\tRESP0:   00 (stall)",
+            "\n\t\tRESP1:   1f"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b0;
+        tb_rindex = 5'h01;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h1f}}};
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     01",
+            "\n\t\tRESP0:   01",
+            "\n\t\tRESP1:   1f"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h01;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h1f}}};
+
+        check_outputs();
+
+        @(posedge CLK); #(PERIOD/10);
+
+        // inputs
+        sub_test_case = {
+            "\n\t\tupdate:  i",
+            "\n\t\tREQ:     01",
+            "\n\t\tRESP0:   01",
+            "\n\t\tRESP1:   01"
+        };
+        $display("\t- sub_test: %s", sub_test_case);
+
+        // reset
+        nRST = 1'b1;
+        tb_ren0 = 1'b1;
+        tb_ren1 = 1'b1;
+        tb_rindex = 5'h01;
+        tb_wen_byte = 4'b0000;
+        tb_windex = 5'h00;
+        tb_wdata = 32'h00000000;
+
+        @(negedge CLK);
+
+        // outputs:
+
+        expected_rdata = {{7{5'h01}}};
+
+        check_outputs();
 
         // ------------------------------------------------------------
         // finish:
